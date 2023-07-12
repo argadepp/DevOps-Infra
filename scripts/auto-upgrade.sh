99752043c57ed0039175f5f7d5ed8d2f02336576
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2023-07-12 10:40:08 +0530

    Update auto-upgrade.sh

--- scripts/auto-upgrade.sh
+++ scripts/auto-upgrade.sh
@@ -20,16 +20,23 @@ run_backup_prom() {
     bash ${WORKSPACE}/scripts/backup-helm-charts.sh "prom" "${prom_version}"
     helm repo update
     echo "Testing Dry Run"
-   # helm upgrade --install thanos $REPO_GIT/thanos --version "${thanos_version}" --namespace utilities --dry-run -o json | jq 
+   # helm upgrade --install thanos $REPO_GIT/thanos --version "${prom_version}" --namespace utilities --dry-run -o json | jq 
 }
 
+run_backup_argo() {
+    bash ${WORKSPACE}/scripts/backup-helm-charts.sh "argo" "${argo_version}"
+    helm repo update
+    echo "Testing Dry Run"
+   # helm upgrade --install thanos $REPO_GIT/argo --version "${argo_version}" --namespace utilities --dry-run -o json | jq 
+}
 
 
 helm repo add thanos https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
+helm repo add argo https://argoproj.github.io/argo-helm
 utilities_list="thanos \
-                prom"
+                prom \
+                argo"
 
 for utility_name in ${utilities_list}; do
     toggle_variable="backup_${utility_name}"
